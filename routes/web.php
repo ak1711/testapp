ca16e4ed445b8a667066ebba27394c72ea352887
A Tripathi <ak.triphathi1711@gmail.com>
2021-05-30 13:27:28 +0530

    final1

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', 'App\Http\Controllers\HomeController@member')->name('member');
 Route::get('/{id}/edit', 'App\Http\Controllers\HomeController@edit');
 Route::post('/store', 'App\Http\Controllers\HomeController@memberstore')->name('memberstore');
